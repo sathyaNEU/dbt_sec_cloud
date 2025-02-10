2dc16b6f4210d430cff419a37c964da6c4647024
Sathyavarthan Balachandar <balachandar.sa@northeastern.edu>
2025-02-10 22:36:22 +0000

    feat: dim_tag

--- models/dw/DIM_COMPANY.sql
+++ models/dw/DIM_COMPANY.sql
@@ -2,9 +2,34 @@
     unique_key=['CIK', 'NAME']
 ) }}
 
+WITH deduplicated AS (
+    SELECT 
+        CIK,
+        NULL AS TICKER,
+        NAME,
+        COUNTRYBA,
+        STPRBA,
+        CITYBA,
+        ZIPBA,
+        BAS1,
+        BAS2,
+        BAPH,
+        COUNTRYMA,
+        STPRMA,
+        CITYMA,
+        ZIPMA,
+        MAS1,
+        MAS2,
+        ACIKS,
+        NCIKS,
+        CURRENT_TIMESTAMP AS CREATED_DT,
+        CURRENT_USER() AS CREATED_BY,
+        ROW_NUMBER() OVER (PARTITION BY CIK, NAME ORDER BY CURRENT_TIMESTAMP) AS row_num
+    FROM raw.sub
+)
 SELECT 
     CIK,
-    NULL AS TICKER,
+    TICKER,
     NAME,
     COUNTRYBA,
     STPRBA,
@@ -21,9 +46,10 @@ SELECT
     MAS2,
     ACIKS,
     NCIKS,
-    CURRENT_TIMESTAMP AS CREATED_DT,
-    CURRENT_USER() AS CREATED_BY
-FROM 
-    raw.sub
+    CREATED_DT,
+    CREATED_BY
+FROM deduplicated
+WHERE row_num = 1
+
 
     
