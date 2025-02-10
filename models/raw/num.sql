43af1c0dd3349e7d2be76ed4f106cf80de7e95cf
balachandar.sa@northeastern.edu <balachandar.sa@northeastern.edu>
2025-02-10 04:41:36 +0000

    raw: pre, tag, num

---
+++ models/raw/num.sql
@@ -0,0 +1,13 @@
+SELECT 
+    VALUE:c1::STRING AS ADSH,         
+    VALUE:c2::STRING AS TAG,          
+    VALUE:c3::STRING AS VERSION,      
+    TO_DATE(VALUE:c4::STRING, 'YYYYMMDD') AS DDATE,                  
+    VALUE:c5::INT AS QTRS,                 
+    VALUE:c6::STRING AS UOM,           
+    VALUE:c7::STRING AS SEGMENTS,     
+    VALUE:c8::STRING AS COREG,       
+    VALUE:c9::INT AS VALUE,      
+    VALUE:c10::STRING AS FOOTNOTE      
+FROM SEC.OBJECTS.SEC_EXT_TABLE
+WHERE METADATA$FILENAME LIKE '%2024/4/num.tsv'
